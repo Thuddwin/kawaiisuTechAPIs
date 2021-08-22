1641888dffd01b34781fab4628832d44808fd1b2
Thuddwin <pangdenanren@hotmail.com>
2021-08-22 20:56:32 +0100

    Changed event name to 'new_schedule'.

--- kTechServer.js
+++ kTechServer.js
@@ -10,17 +10,17 @@ const https_options = {
 const myDeviceName = 'kTechServer';
 
 // API ACCESS /////////////////////////////////////////
-const https = require('https');
-let server = https.createServer(https_options, app)
-server.listen(443);
+    const https = require('https');
+    let server = https.createServer(https_options, app)
+    server.listen(443);
 // EXPRESS SETUP //
-app.use(express.static('public'));
+    app.use(express.static('public'));
 // WEB PAGE SETUP (Don't put in the API?) //
-app.get('/', (req, res) => {
-        res.status(200).send('Future site of KawaiisuTech.com.');
-});
+    app.get('/', (req, res) => {
+            res.status(200).send('Future site of KawaiisuTech.com.');
+    });
 // API PULL DATA REQUEST (Requires effort from the Client.) //
-app.get('/schedule', (req, res) => {
+    app.get('/schedule', (req, res) => {
     // First, read schedule from disk.
     fa.gs().then((sched) => {
         console.log(`${myDeviceName}:app.get/schedule:fa.gs():sched pull...`);
@@ -32,38 +32,42 @@ app.get('/schedule', (req, res) => {
 ///////////////////////////////////////////////////////
 
 // NON-SECURE SOCKET ACCESS ///////////////////////////
-const http = require('http');
-const io = require('socket.io')(http);
-io.listen(56010);
+    const http = require('http');
+    const io = require('socket.io')(http);
+    io.listen(56010);
 ///////////////////////////////////////////////////////
 
 // SOCKET CONNECTIONS //
-io.on('connection', socket => {
-    console.log(`${myDeviceName}:io.on:connection: Device connected.`)
-    socket.emit('ktech_sends_message', {'message':'connected_to_ktech','data': 'Connected to kTechServer @ http://www.kawaiisutech.com.'});
-    
-    // INCOMING MESSAGES //
-    socket.on('client_sends_message_to_ktech', data => {
-        let {message} = data;
-        if(message === 'request_api_schedule') {
-            getScheduleAsync();
-        } else if (message === 'my_id') {
-            let {id} = data;
-            getScheduleAsync();
+    io.on('connection', socket => {
+        console.log(`${myDeviceName}:io.on:connection: Device connected.`)
+        socket.emit('ktech_sends_message', {'message':'connected_to_ktech','data': 'Connected to kTechServer @ http://www.kawaiisutech.com.'});
+        
+        // INCOMING MESSAGES //
+        socket.on('client_sends_message_to_ktech', data => {
+            let {message} = data;
+            if(message === 'request_api_schedule') {
+                getScheduleAsync();
+            } else if (message === 'my_id') {
+                let {id} = data;
+                console.log(`${myDeviceName}:Client ID: ${id}.`);
+                getScheduleAsync();
+            }
+        });
+    });
+//////////////////////////
+
+// NOTIFICATIONS /////////
+    notifier.on('file_access_sends_message', data => {
+        console.log(`${myDeviceName}:notifier.on:file_access_sends_message`);
+        if(data.message === 'new_schedule') {
+            io.emit('ktech_sends_message', {'message':'new_schedule', 'data':data.data});
         }
     });
-});
-
-notifier.on('file_access_sends_message', data => {
-    console.log(`${myDeviceName}:notifier.on:file_access_sends_message`);
-    if(data.message === 'new_schedule') {
-        io.emit('ktech_sends_message', {'message':'new_schedule', 'data':data.data});
-    }
-});
+//////////////////////////
 
 async function getScheduleAsync() {
     return await fa.getSchedJSON().then((sched) => {
-        io.emit('ktech_sends_message', {'message':'requested_api_schedule', 'data': sched});
+        io.emit('ktech_sends_message', {'message':'new_schedule', 'data': sched});
         return sched;
     });
 }
